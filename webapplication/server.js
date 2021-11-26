0fe7dff203ed199dd57c882b0dcc6cd0d9ffcded
Marse9713 <raffaele.marseu@outlook.it>
2021-11-26 12:03:04 +0100

    modifiche per controllo routes

--- webapplication/server.js
+++ webapplication/server.js
@@ -4,6 +4,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin:"http://localhost:5500"}));
 
-require("./configuration/routes/routes.js")(app);
+require("../webapplication/configuration/routes/routes.js")(app);
 
-app.listen(5000);
+app.listen(5000); //listen on 5000
