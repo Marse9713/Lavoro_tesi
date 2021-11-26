ad715d76fe1dd0054d31671c311c99b80a3d745d
Marse9713 <raffaele.marseu@outlook.it>
2021-11-26 11:47:45 +0100

    cambiamento per eliminazione di server

--- webapplication/server.js
+++ webapplication/server.js
@@ -4,6 +4,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin:"http://localhost:5500"}));
 
-require("./routes/routes.js")(app);
+require("./configuration/routes/routes.js")(app);
 
 app.listen(5000);
